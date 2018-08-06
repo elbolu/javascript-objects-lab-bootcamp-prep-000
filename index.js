e68afe7cc70c0bf1a32611300052954d3e7d1983
Leah Hill <leah@verysandwich.com>
2018-08-06 23:30:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(recipes, key, value) {
+      return Object.assign({}, recipes, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value
+  return recipes
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  var newRecipes = Object.assign ({}, recipes)
+  delete newRecipes.key
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  var newRecipes = Object.assign ({}, recipes)
+  delete newRecipes.key
+}
