ae9632f1814d9ba6a28003d6d68493fa42deba77
Leah Hill <leah@verysandwich.com>
2018-08-07 00:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  const newObj = Object.assign ({}, object)
-  delete newObj.key
+  const newObj = Object.assign({}, object)
+  delete newObj[key]
   return newObj
 }
 
