ef1e54c0505715777b790a16cd1e7dc788fa91fb
Leah Hill <leah@verysandwich.com>
2018-08-07 00:00:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  objet[key] = value
+  object[key] = value
   return object
 }
 
